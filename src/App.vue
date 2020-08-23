1235ef15bfd751e1396f7272ef00ad077d2393dc
AndrewGardhouse <AndrewGardhouse@gmail.com>
2020-08-23 12:34:16 -0400

    Creates layout

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,29 @@
       <h1 class="title__sk8">SK8</h1>
       <h1 class="title__dice">DICE</h1>
     </div>
+    <div class="dice">
+      <Die :sides="[]"></Die>
+      <Die :sides="[]"></Die>
+      <Die :sides="[]"></Die>
+      <Die :sides="[]"></Die>
+    </div>
+    <div class="tricklist">
+      <h2 class="tricklist__title">Trick List</h2>
+    </div>
+    <div class="rolldice-button">
+      <button type="button" name="button">Roll 'Em</button>
+    </div>
   </div>
 </template>
 
 <script>
+import Die from './components/Die.vue';
 
 export default {
   name: 'App',
+  components: {
+    Die,
+  },
 };
 </script>
 
---
+++ src/components/Die.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="die">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    sides: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.die {
+  background: red;
+  color: white;
+  border-radius: 25px;
+}
+</style>
