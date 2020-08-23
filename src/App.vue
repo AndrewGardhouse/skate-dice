72b258369096fa664a68639870f3b106c1c19cfa
AndrewGardhouse <AndrewGardhouse@gmail.com>
2020-08-23 11:17:43 -0400

    Adds title

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
+    <div class="title">
+      <h1 class="title__sk8">SK8</h1>
+      <h1 class="title__dice">DICE</h1>
+    </div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
 };
 </script>
+
+<style lang="scss">
+@import "./assets/style";
+</style>
