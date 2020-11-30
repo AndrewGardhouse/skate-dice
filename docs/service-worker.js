9eb8451b6ffbf75a85c265e253313d46c8cb318b
AndrewGardhouse <AndrewGardhouse@gmail.com>
2020-11-30 14:24:20 -0500

    Updates service worker

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("https://sk8diceapp.com/precache-manifest.3faf73e41cc668894d6200d71d7bd9da.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts('https://sk8diceapp.com/precache-manifest.3faf73e41cc668894d6200d71d7bd9da.js', 'https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 /* eslint-disable no-undef */
 /* eslint-disable no-restricted-globals */
@@ -8,15 +8,11 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 workbox.core.setCacheNameDetails({ prefix: 'sk8-dice' });
 
 workbox.routing.registerRoute(
-  /^https:\/\/fonts.(gstatic|googleapis).com(.*)/,
-  new workbox.strategies.CacheFirst({
-    cacheName: 'google-fonts-webfonts',
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'googleapis',
     plugins: [
-      new workbox.cacheableResponse.Plugin({
-        statuses: [0, 200],
-      }),
       new workbox.expiration.Plugin({
-        maxAgeSeconds: 60 * 60 * 24 * 365,
         maxEntries: 30,
       }),
     ],
@@ -29,4 +25,3 @@ self.addEventListener('message', (msg) => {
     self.skipWaiting();
   }
 });
-
