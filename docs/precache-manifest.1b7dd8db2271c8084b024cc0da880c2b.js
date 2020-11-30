b41c64fee5739e4b4713df3dc60ad5a7f647df5d
AndrewGardhouse <AndrewGardhouse@gmail.com>
2020-11-30 09:06:00 -0500

    Build project

---
+++ docs/precache-manifest.1b7dd8db2271c8084b024cc0da880c2b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef5aead60c196160206ca75fa1641e2",
+    "url": "https://sk8diceapp.com/CNAME"
+  },
+  {
+    "revision": "6c813ced9ced3036d663",
+    "url": "https://sk8diceapp.com/css/app.1a57a3e5.css"
+  },
+  {
+    "revision": "5f8acfa1225a136a52cd7933e87c034c",
+    "url": "https://sk8diceapp.com/favicon.ico"
+  },
+  {
+    "revision": "136657ffc46504162bfc939e4092dc0a",
+    "url": "https://sk8diceapp.com/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "6f752f418a3c479158d9a7ca0dfdbd7f",
+    "url": "https://sk8diceapp.com/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "136657ffc46504162bfc939e4092dc0a",
+    "url": "https://sk8diceapp.com/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "6f752f418a3c479158d9a7ca0dfdbd7f",
+    "url": "https://sk8diceapp.com/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "7df81337c8737a6932466e3868b90f64",
+    "url": "https://sk8diceapp.com/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "d1e36583a1a0ce7865cee512ff274152",
+    "url": "https://sk8diceapp.com/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "d7dc8cd77dd766ed3383fea47ce61306",
+    "url": "https://sk8diceapp.com/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "dcf8c221d8cc0dd1d6a9048bec7e0288",
+    "url": "https://sk8diceapp.com/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "a2352a294949d86dfce48ad24469c467",
+    "url": "https://sk8diceapp.com/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "d7dc8cd77dd766ed3383fea47ce61306",
+    "url": "https://sk8diceapp.com/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "46e09c8254e91ea4e08e10daa193861e",
+    "url": "https://sk8diceapp.com/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2e9412ee789be8a339c6d1834db7e405",
+    "url": "https://sk8diceapp.com/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "e64290ad3d7ea47b66aa1390cf680cdb",
+    "url": "https://sk8diceapp.com/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "17617af0e110448004020cf86bf1887d",
+    "url": "https://sk8diceapp.com/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "26152b8f40b044a64da41ef3a4e446f0",
+    "url": "https://sk8diceapp.com/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "bb1ae1a65c7bda4672f53b6242abffb0",
+    "url": "https://sk8diceapp.com/index.html"
+  },
+  {
+    "revision": "6c813ced9ced3036d663",
+    "url": "https://sk8diceapp.com/js/app.d081120a.js"
+  },
+  {
+    "revision": "dbb18151ae768488399e",
+    "url": "https://sk8diceapp.com/js/chunk-vendors.3ccd087f.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://sk8diceapp.com/robots.txt"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("https://sk8dice.app/precache-manifest.a472cd7c45ef1a2b499bedf241603c27.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://sk8diceapp.com/precache-manifest.1b7dd8db2271c8084b024cc0da880c2b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* eslint-disable no-undef */
 /* eslint-disable no-restricted-globals */
